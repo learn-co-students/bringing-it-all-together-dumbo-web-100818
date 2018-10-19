50966aa5b898752f053b84e7eeecfecfd33c18b4
said shah <saidmysr20@aol.com>
2018-10-19 19:38:00 -0400

    added most sql statements

---
+++ lib/dog.rb
@@ -0,0 +1,53 @@
+  require "pry"
+class Dog
+
+  attr_accessor :id, :name, :breed
+
+  def initialize(id: nil, name: , breed: )
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    DB[:conn].execute("DROP TABLE dogs")
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed) VALUES (?, ?)
+    SQL
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+      Dog.new(id: @id, name: name, breed: breed)
+  end
+
+  def self.create(given_attributes)
+      Dog.new(name: given_attributes[:name], breed: given_attributes[:breed]).save
+  end
+
+  def self.find_by_id(id)
+      sql = <<-SQL
+          SELECT * FROM dogs WHERE id = ?
+      SQL
+
+      DB[:conn].execute(sql, id).map do |key|
+      return  Dog.new(id: key[0], name: key[1], breed: key[2])
+    end
+  end
+
+
+
+
+end
